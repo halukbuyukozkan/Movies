0201a183c7370ecaae218eb60e09dbf3b9f9fb76
halukbuyukozkanü <halukbuyukozkan@gmail.com>
2023-09-11 15:27:33 +0300

    singleMovie query created

--- src/main/java/www/buyukozkan/movies/MovieRepository.java
+++ src/main/java/www/buyukozkan/movies/MovieRepository.java
@@ -1,10 +1,14 @@
 package www.buyukozkan.movies;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    // automatic query generation
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
-```
+
